97cb21eac7fba3c6afd59ce64f865c1a7024f2a0
Racinettee <StevePhillipsCa@gmail.com>
2016-10-09 11:29:58 -0700

    Add constructor taking source buffer

--- source/SourceEditor.d
+++ source/SourceEditor.d
@@ -2,12 +2,19 @@ module coral.SourceEditor;
 
 import gtk.ScrolledWindow;
 import gsv.SourceView;
+import gsv.SourceBuffer;
 
 class SourceEditor : ScrolledWindow
 {
 	this()
 	{
 		editor = new SourceView();
+		add(editor);
+		showAll();
+	}
+	this(SourceBuffer sb)
+	{
+		editor = new SourceView(sb);
 		add(editor);
 		showAll();
 	}
