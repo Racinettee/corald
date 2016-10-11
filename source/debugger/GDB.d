b456c83295e25ca6c93a606762d60c6cc607e107
Racinettee <StevePhillipsCa@gmail.com>
2016-10-10 23:31:47 -0700

    Preliminary implementation of gdb debugger interface

--- source/debugger/GDB.d
+++ source/debugger/GDB.d
@@ -1,12 +1,41 @@
 ﻿module coral.debugger.GDB;
 
+import std.process;
+
 import coral.debugger.IDebugger;
 
 class GDB : IDebugger
 {
-  this()
+  this(string executable)
   {
-    // Constructor code
+    pipes = pipeProcess(["gdb", "--interpreter=mi", executable]);
   }
+
+  final void start()
+  {
+    pipes.stdin.writeln("r");
+  }
+
+  final void stop()
+  {
+    pipes.stdin.writeln("q\n", "y");
+  }
+
+  final void stepIn()
+  {
+    pipes.stdin.writeln("s");
+  }
+
+  final void stepOver()
+  {
+    pipes.stdin.writeln("n");
+  }
+
+  final void stepOut()
+  {
+    pipes.stdin.writeln("f");
+  }
+
+  ProcessPipes pipes;
 }
 
