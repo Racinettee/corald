c1876201d38dfa11d6557e7d2f928ee707562c1d
Racinettee <StevePhillipsCa@gmail.com>
2016-11-06 23:29:03 -0800

    Add push instance function

--- source/coral/lua/UserData.d
+++ source/coral/lua/UserData.d
@@ -11,7 +11,7 @@ import coral.lua.Lua;
 
 T toType(T)(lua_State* L, int index)
 {
-  T obj = cast(T)lua_touserdata(L, index);
+  T* obj = cast(T*)lua_touserdata(L, index);
   if(obj == null)
     luaL_typeerror(L, index, 
       toStringz(fullyQualifiedName!T));
@@ -29,60 +29,17 @@ T checkType(T)(lua_State* L, int index)
   return obj;
 }
 
-void pushClassInstance(T)(lua_State* L, T obj) if (is(T == class))
+void pushInstance(T)(lua_State* state, T instance, luaL_Reg[] methodTable)
 {
-	T* ud = cast(T*)lua_newuserdata(L, obj.sizeof);
-	*ud = obj;
+	T* t = cast(AppWindow*)lua_newuserdata(state, instance.sizeof);
+	*t = instance;
 
-	pushMeta(L, obj);
-	lua_setmetatable(L, -2);
-
-	GC.addRoot(ud);
-}
-
-private void pushMeta(T)(lua_State* L, T obj)
-{
-	if(luaL_newmetatable(L, T.mangleof.ptr) == 0)
-		return;
-
-	pushValue(L, T.stringof);
-	lua_setfield(L, -2, "__dclass");
-
-	pushValue(L, T.mangleof);
-	lua_setfield(L, -2, "__dmangle");
-
-	lua_newtable(L); //__index fallback table
-
-	foreach(member; __traits(derivedMembers, T))
+	if(luaL_newmetatable(state, metatableName))
 	{
-		static if(__traits(getProtection, __traits(getMember, T, member)) == "public" && //ignore non-public fields
-			member != "this" && member != "__ctor" && //do not handle
-			member != "Monitor" && member != "toHash" && //do not handle
-			member != "toString" && member != "opEquals" && //handle below
-			member != "opCmp") //handle below
-		{
-			static if(__traits(getOverloads, T.init, member).length > 0 && !__traits(isStaticFunction, mixin("T." ~ member)))
-			{
-				pushMethod!(T, member)(L);
-				lua_setfield(L, -2, toStringz(member));
-			}
-		}
+		lua_pushvalue(state, -1);
+		lua_setfield(state, -2, "__index");
+		luaL_setfuncs(state, methodTable.ptr, 0);
+		lua_setmetatable(state, -2);
 	}
 
-	lua_setfield(L, -2, "__index");
-
-	pushMethod!(T, "toString")(L);
-	lua_setfield(L, -2, "__tostring");
-
-	pushMethod!(T, "opEquals")(L);
-	lua_setfield(L, -2, "__eq");
-
-	//TODO: handle opCmp here
-
-
-	lua_pushcfunction(L, &classCleaner);
-	lua_setfield(L, -2, "__gc");
-
-	lua_pushvalue(L, -1);
-	lua_setfield(L, -2, "__metatable");
 }
