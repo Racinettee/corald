1dabfbc4843c351316acad8d4c51c8ad6f7c9e8c
Racinettee <Racinettee@outlook.com>
2017-02-12 13:50:25 -0800

    Going to try and expose class to lua

--- source/coral/lua/classes.d
+++ source/coral/lua/classes.d
@@ -3,39 +3,28 @@ module coral.lua.classes;
 import std.stdio;
 import std.traits;
 
+import coral.lua.c.all;
 import coral.lua.state;
 import coral.lua.attrib;
 
 void registerClass(T)(State state)
 {
-    pragma(msg, __traits(getAttributes, T));
-    static assert(hasUDA!(T.openFile, LuaExport));
     static assert(hasUDA!(T, LuaExport));
-    static assert(getUDAs!(T, LuaExport)[0].name == "AppWindow");
-    writeln("There are: ", getUDAs!(T, LuaExport).length, " udas for this class");
-       // foreach(member; getUDAs!(T, LuaExport))
-   //     writeln(member.name);
-   iterateUDAMembers!(T, 0);
 
-    /*foreach(member; __traits(derivedMembers, T))
-    {
-        pragma(msg, __traits(getAttributes, member));
-        //static if(__traits(getProtection, mixin("T."~member)) == "public")
-          //  write(mixin("T."~member).mangleof, " member: ", getUDAs!(mixin("T."~member), LuaExport));
-        static if(__traits(getProtection, mixin("T."~member)) == "public" && hasUDA!(mixin("T."~member), LuaExport))
-        {
-            foreach(memberUDA; getUDAs!(mixin("T."~member), LuaExport))
-                writeln(member~": ", memberUDA.name);
-        }
-    }*/
+    lua_State* L = state.state;
+
+    luaL_newmetatable(L, "luaL_"~T.stringof);
+
+    iterateUDAMembers!(T, 0);
 }
 
 void iterateUDAMembers(T, uint index)()
 {
-    static if(__traits(getProtection, mixin("T."~__traits(derivedMembers, T)[index])) == "public")
-        pragma(msg, __traits(derivedMembers, T)[index]);
     static if(__traits(getProtection, mixin("T."~__traits(derivedMembers, T)[index])) == "public" && hasUDA!(mixin("T."~__traits(derivedMembers, T)[index]), LuaExport))
-        pragma(msg, "Found a member with uda");
+    {
+        pragma(msg, "Found a member with uda "~__traits(derivedMembers, T)[index]);
+
+    }
     
     static if(index + 1 < __traits(derivedMembers, T).length)
         iterateUDAMembers!(T, index+1);
