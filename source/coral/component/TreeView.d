8d2711274bc2678dc06d3f16fff31e271c6b6b0e
Racinettee <Racinettee@outlook.com>
2017-04-02 17:20:00 -0700

    Will start work on tree store

--- source/coral/component/TreeView.d
+++ source/coral/component/TreeView.d
@@ -1,2 +1,14 @@
-import gtk.TreeStore;
+module coral.component.treeview;
 
+import gtk.TreeStore;
+import gtkc.gobjecttypes : GType;
+import gtkc.gtktypes : GtkImageType;
+
+class TreeView
+{
+  public this()
+  {
+   //store = new TreeStore([GtkImageType.PIXBUF, GType.STRING]);
+  }
+  TreeStore store;
+}
