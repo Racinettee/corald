ac437d47aeb34aa1481b82055398536cb5ffe14b
Racinettee <Racinettee@outlook.com>
2017-05-29 00:32:47 -0700

    Feature is activated through menu item

--- script/filetree.lua
+++ script/filetree.lua
@@ -2,40 +2,56 @@ lgi = require 'lgi'
 lfs = require 'lfs'
 Gtk = lgi.require('Gtk')
 
-function print_table(tbl)
-    for k,v in pairs(tbl) do
-       print(k, v)
-    end
-end
-
 function editor_created(window)
-    local ftree = ScrolledFileTree.new(lfs.currentdir() .. '/script')
-    local fstore = Gtk.TreeStore(ftree.store)
-    local scrolled_window = Gtk.ScrolledWindow(ftree.window)
-    local tree_view = scrolled_window:get_child()
-    function tree_view:on_row_activated(treePath, treeColumn)
-        local function build_path_to_file(row)
-            if row == nil then
-                return ''
+    local menu_bar = Gtk.MenuBar(window.menubar)
+    menu_bar:get_children()[1]:get_submenu():insert(Gtk.MenuItem {
+        label = 'Open Path',
+        on_activate = function()
+            local path_chooser = Gtk.FileChooserDialog {
+                title = 'Select Folder',
+                buttons = {
+                    { Gtk.STOCK_OK, Gtk.ResponseType.ACCEPT },
+                    { Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL },
+                },
+                action = 'SELECT_FOLDER'
+            }
+            local path = ''
+            if path_chooser:run() == Gtk.ResponseType.Cancel then
+                return
             end
-            if fstore:iter_parent(row) == nil then
-                return ''
+            path = path_chooser:get_filename()
+            path_chooser:destroy()
+            if path == '' then
+                return
             end
-            return build_path_to_file(fstore:iter_parent(row)) ..
-                '/' .. fstore:get_value(row, 1):get_string()
+            local ftree = ScrolledFileTree.new(path)
+            local fstore = Gtk.TreeStore(ftree.store)
+            local scrolled_window = Gtk.ScrolledWindow(ftree.window)
+            local tree_view = scrolled_window:get_child()
+            function tree_view:on_row_activated(treePath, treeColumn)
+                local function build_path_to_file(row)
+                    if row == nil then
+                        return ''
+                    end
+                    if fstore:iter_parent(row) == nil then
+                        return ''
+                    end
+                    return build_path_to_file(fstore:iter_parent(row)) ..
+                        '/' .. fstore:get_value(row, 1):get_string()
+                end
+                window:openFile(ftree:get_path() .. build_path_to_file(fstore:get_iter(treePath)))
+            end
+            local moonWindow = Gtk.Window {
+                title = 'moon',
+                default_width = 400,
+                default_height = 300
+            }
+            moonWindow:add(scrolled_window)
+            moonWindow:show()
         end
-        print(treePath, treeColumn)
-        window:openFile(ftree:get_path() .. build_path_to_file(fstore:get_iter(treePath)))
-    end
-    
-    local moonWindow = Gtk.Window {
-        title = 'moon',
-        default_width = 400,
-        default_height = 300
-    }
-    moonWindow:add(scrolled_window)
-    moonWindow:show()
+    }, 2)
 end
+-- Return the callbacks to the plugin system
 return {
     on_editor_created = editor_created
 }
